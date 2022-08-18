5093808f5c5488a8e2c3444f0edaa327484ed4c4
CSaintos <48599958+CSaintos@users.noreply.github.com>
2022-08-17 18:24:47 -0700

    SampleWindow.cpp
    Finished linking, tasks.json, launch.json, and SampleWindow.cpp

---
+++ cmd/SampleWindow.cmd
@@ -0,0 +1,17 @@
+::@echo off
+
+SET includes=-Iapp\inc -Ilib\GLFW -Ilib\glm -Ilib\Vulkan\Include
+SET links=-Llib\Vulkan\Lib -Llib\GLFW -lvulkan-1 -l:libglfw3.a -lgdi32
+SET defines=
+
+echo "clean"
+del build\SampleWindow.exe
+
+echo "compile"
+g++ %includes% -c app\src\SampleWindow.cpp -o bin\sampleWindow.o -g
+
+echo "build"
+g++ bin\sampleWindow.o %links% -o build\SampleWindow.exe -g
+
+echo "obj-clean"
+del bin\*.o /Q /F
