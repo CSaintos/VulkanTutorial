12e66e56f9d0b9e62af35a886337738f373412a6
CSaintos <48599958+CSaintos@users.noreply.github.com>
2022-09-23 15:55:30 -0700

    Created command pool, created command buffer and allocated it on command pool, recorded the render pass onto the command buffer, wrote the triangle draw command onto the buffer by setting its viewport and scissor, and finally wrote the command buffer closer

--- app/src/HelloTriangle.cpp
+++ app/src/HelloTriangle.cpp
@@ -31,6 +31,8 @@ struct VkContext
   VkPipelineLayout pipeline_layout;
   VkPipeline graphics_pipeline;
   std::vector<VkFramebuffer> swap_chain_framebuffers;
+  VkCommandPool command_pool;
+  VkCommandBuffer command_buffer;
 };
 
 struct QueueFamilyIndices
@@ -686,6 +688,84 @@ private:
 
   }
 
+  void createCommandPool()
+  {
+    QueueFamilyIndices queue_family_indices = findQueueFamilies(context.physical_device);
+
+    VkCommandPoolCreateInfo pool_info{};
+    pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    pool_info.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    pool_info.queueFamilyIndex = queue_family_indices.graphics_family.value();
+
+    if (vkCreateCommandPool(context.device, &pool_info, nullptr, &context.command_pool) != VK_SUCCESS)
+    {
+      throw std::runtime_error("Failed to create command pool!");
+    }
+  }
+
+  void recordCommandBuffer(VkCommandBuffer command_buffer, uint32_t image_index)
+  {
+    VkCommandBufferBeginInfo buffer_begin_info{};
+    buffer_begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    buffer_begin_info.flags = 0; // Optional
+    buffer_begin_info.pInheritanceInfo = nullptr; // Optional
+
+    if (vkBeginCommandBuffer(context.command_buffer, &buffer_begin_info) != VK_SUCCESS)
+    {
+      throw std::runtime_error("Failed to begin recording command buffer!");
+    }
+
+    VkRenderPassBeginInfo render_pass_info{};
+    render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    render_pass_info.renderPass = context.render_pass;
+    render_pass_info.framebuffer = context.swap_chain_framebuffers[image_index];
+    render_pass_info.renderArea.offset = {0, 0};
+    render_pass_info.renderArea.extent = context.swap_chain_extent;
+
+    VkClearValue clear_color = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+    render_pass_info.clearValueCount = 1;
+    render_pass_info.pClearValues = &clear_color;
+
+    vkCmdBeginRenderPass(command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, context.graphics_pipeline);
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = static_cast<float>(context.swap_chain_extent.width);
+    viewport.height = static_cast<float>(context.swap_chain_extent.height);
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+    vkCmdSetViewport(command_buffer, 0, 1, &viewport);
+
+    VkRect2D scissor{};
+    scissor.offset = {0, 0};
+    scissor.extent = context.swap_chain_extent;
+    vkCmdSetScissor(command_buffer, 0, 1, &scissor);
+
+    vkCmdDraw(command_buffer, 3, 1, 0, 0);
+
+    vkCmdEndRenderPass(command_buffer);
+
+    if (vkEndCommandBuffer(command_buffer) != VK_SUCCESS) {
+      throw std::runtime_error("Failed to record command buffer!");
+    }
+  }
+
+  void createCommandBuffer()
+  {
+    VkCommandBufferAllocateInfo buffer_alloc_info{};
+    buffer_alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    buffer_alloc_info.commandPool = context.command_pool;
+    buffer_alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    buffer_alloc_info.commandBufferCount = 1;
+
+    if (vkAllocateCommandBuffers(context.device, &buffer_alloc_info, &context.command_buffer) != VK_SUCCESS)
+    {
+      throw std::runtime_error("Failed to allocate command buffers!");
+    }
+  }
+
   void initVulkan()
   {
     createInstance();
@@ -697,6 +777,8 @@ private:
     createRenderPass();
     createGraphicsPipeline();
     createFramebuffers();
+    createCommandPool();
+    createCommandBuffer();
   }
 
   void mainLoop()
@@ -709,6 +791,8 @@ private:
 
   void cleanup()
   {
+    vkDestroyCommandPool(context.device, context.command_pool, nullptr);
+
     for (VkFramebuffer framebuffer : context.swap_chain_framebuffers)
     {
       vkDestroyFramebuffer(context.device, framebuffer, nullptr);
